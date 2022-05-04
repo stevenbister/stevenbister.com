cf191d567466a28896ddceac7119bce0c0b258b5
Steven Bister <stevebister@gmail.com>
2022-05-04 13:17:16 +0100

    Adds social media icons

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,16 +1,12 @@
+import SocialIcons from './SocialIcons';
+
 const HomeContent = ({ intro, links }) => {
   return (
     <div className="container cover">
       <div className="stack align-center">
         <p className="content">{intro}</p>
 
-        <ul className="content d-flex">
-          {Object.entries(links).map(([key, value]) => (
-            <li key={key}>
-              <a href={value}>{key}</a>
-            </li>
-          ))}
-        </ul>
+        <SocialIcons links={links} />
       </div>
     </div>
   );
---
+++ src/components/SocialIcons.js
@@ -0,0 +1,19 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+const SocialIcons = ({ links }) => {
+  return (
+    <ul
+      className="content d-flex justify-center fs-2"
+      style={{ '--stack-space': 'var(--space-s-m)' }}
+    >
+      {Object.entries(links).map(([key, value]) => (
+        <li key={key} style={{ marginInline: 'var(--space-2xs)' }}>
+          <a href={value} style={{ color: 'var(--color-text-primary)' }}>
+            {key === 'LinkedIn' ? <FaLinkedin /> : <FaGithub />}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+export default SocialIcons;
